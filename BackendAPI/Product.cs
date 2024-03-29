99d03b91580ccf0b64bb063cec5418100dd0115b
valle <valle@solutionstreet.com>
2024-03-29 13:14:47 -0400

    Add project files.

---
+++ BackendAPI/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace AngularSampleProject2.Server
+{
+    public class Product
+    {
+        public string? id { get; set; }
+        public string? title { get; set; }
+        public decimal? price { get; set; }
+        public string? description { get; set; }
+        public string? image { get; set; }
+        public string? category { get; set; }
+        public Rating? Rating { get; set; }
+    }
+
+
+    public class Rating
+    {
+        public decimal? rate { get; set; }
+        public int? count { get; set; }
+    }
+}
